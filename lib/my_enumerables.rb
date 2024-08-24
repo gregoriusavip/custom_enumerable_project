99e371a5e1c5e5ace1675dd9555162abd6307dd5
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-08-23 19:49:46 -0700

    Fix my_each and implement my_all? using it
    my_each should return self if block is given instead of returning the
    modified self from the each block.

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,21 @@
 module Enumerable
   # Your code goes here
+  def my_all?(arg = (no_var = true; nil))
+    return true if length.eql?(0)
+
+    unless arg.nil? && no_var
+      my_each { |elem| return false unless arg === elem }
+      return true
+    end
+
+    unless block_given?
+      my_each { |elem| return false if !!elem }
+      return true
+    end
+
+    my_each { |elem| return false unless yield(elem) }
+    true
+  end
 end
 
 # You will first have to define my_each
@@ -8,6 +24,8 @@ end
 # to this method
 class Array
   def my_each
-    block_given? ? map { |elem| yield(elem) || elem } : to_enum
+    return to_enum unless block_given?
+    length.times { |i| yield(self[i]) }
+    self
   end
 end
