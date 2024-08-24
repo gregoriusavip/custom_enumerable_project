84ffdcba7c7fc489f539bddfe3368c74360f122b
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-08-23 18:45:49 -0700

    Add my_each

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,7 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    block_given? ? map { |elem| yield(elem) || elem } : to_enum
+  end
 end
